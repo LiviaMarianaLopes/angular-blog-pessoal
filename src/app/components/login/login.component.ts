26d1bc5bbf1702ca4b7e5af3bf9b3b362e70e840
Lívia Mariana Lopes <liviamariana2004@gmail.com>
2025-05-04 12:20:07 -0300

    :sparkles: feat: login

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+  loginError: string = ''; 
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  login() {
+    if (this.loginForm.valid) {
+      const { username, password } = this.loginForm.value;
+
+      this.authService.login(username, password).subscribe({
+        next: (response) => {
+          localStorage.setItem('token', response.token);
+          this.router.navigate(['/posts']);
+        },
+        error: (error) => {
+          this.loginError = 'Credenciais inválidas. Tente novamente.';
+        }
+      });
+    }
+  }
+}
